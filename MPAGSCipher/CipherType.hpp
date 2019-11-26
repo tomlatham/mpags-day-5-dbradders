a705dcdf1551aff6addb903c31e9819c0c870977
David Bradley <d.bradley.1@warwick.ac.uk>
2019-11-26 14:26:42 +0000

    Update CMakeLists, CipherType

--- MPAGSCipher/CipherType.hpp
+++ MPAGSCipher/CipherType.hpp
@@ -12,7 +12,8 @@
  */
 enum class CipherType {
   Caesar,   ///< The Caesar cipher
-  Playfair  ///< The Playfair cipher
+  Playfair,  ///< The Playfair cipher
+  Vigenere ///< The Vigenere cipher
 };
 
 #endif // MPAGSCIPHER_CIPHERTYPE_HPP 
--- MPAGSCipher/VigenereCipher.cpp
+++ MPAGSCipher/VigenereCipher.cpp
@@ -8,7 +8,8 @@ void VigenereCipher::setKey(const std::string& key)
 
 VigenereCipher::VigenereCipher(const std::string& key)
 {
-
+  // Set the given key
+  setKet(key);
 }
 
 std::string VignenereCipher::applyCipher(const std::string& inputText,
